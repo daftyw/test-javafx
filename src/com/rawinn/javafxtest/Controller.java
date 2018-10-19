e3c08046f4d0d8ff43344ed550bc9c53ceb4fceb
Rawin Ngamloet <rawin.n@kbtg.tech>
2018-10-19 11:07:21 +0700

    v.1

---
+++ src/com/rawinn/javafxtest/Controller.java
@@ -0,0 +1,10 @@
+package com.rawinn.javafxtest;
+
+import javafx.fxml.FXML;
+
+public class Controller {
+    @FXML
+    private void print() {
+        System.out.println("Test controller");
+    }
+}
---
+++ src/com/rawinn/javafxtest/Main.java
@@ -0,0 +1,23 @@
+package com.rawinn.javafxtest;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        GridPane root = FXMLLoader.load(getClass().getResource("main.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
